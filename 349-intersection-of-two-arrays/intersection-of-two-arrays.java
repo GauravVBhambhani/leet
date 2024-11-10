bd3a1c81fb910a8c606838bc1764cc4406a3bdc5
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2024-11-09 19:06:30 -0500

    Time: 4 ms (34.01%) | Memory: 42.5 MB (98.49%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,22 +1,28 @@
 class Solution {
+
+    public static boolean existsInArray(int[] nums2, int element) {
+        for (int num: nums2) {
+            if (num == element) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public int[] intersection(int[] nums1, int[] nums2) {
         
-        Set<Integer> a = new HashSet<>();
+        List<Integer> res = new ArrayList<>();
+
         for (int i = 0; i < nums1.length; i++) {
-            a.add(nums1[i]);
-        }
-        
-        Set<Integer> b = new HashSet<>();
-        for (int i = 0; i < nums2.length; i++) {
-            b.add(nums2[i]);
+            if (!res.contains(nums1[i]) && existsInArray(nums2, nums1[i])) {
+                res.add(nums1[i]);
+            }
         }
 
-        // Set<Integer> intersection = new HashSet<>(a);
-        a.retainAll(b);
-
-        // int[] res = new int[intersection.size()];
-        return a.stream().mapToInt(Integer::intValue).toArray();
-
-        // return res;
+        int[] intersection = new int[res.size()];
+        for (int e = 0; e < res.size(); e++) {
+            intersection[e] = res.get(e);
+        }
+        return intersection;
     }
 }
