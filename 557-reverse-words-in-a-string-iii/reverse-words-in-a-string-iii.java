6dcb1f78f71c0a347a9bd41eafcaa33f84f6014f
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2024-11-13 17:21:06 -0500

    Time: 8 ms (38.34%) | Memory: 45.6 MB (19.28%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,28 +1,31 @@
 class Solution {
     public String reverseWords(String s) {
         
-        String[] sl = s.split(" ");
-        StringBuilder res = new StringBuilder();
+        String[] strs = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
 
-        for (int j = 0; j < sl.length; j++) {
-            // System.out.println(str);
-            char[] ca = sl[j].toCharArray();
-            for (int i = 0; i < ca.length/2; i++) {
-                char fChar = ca[i];
-                char lChar = ca[ca.length - 1 - i];
+        for (String str: strs) {
+            char[] sc = str.toCharArray();
 
-                ca[i] = lChar;
-                ca[ca.length - 1 - i] = fChar; 
+            for (int i = 0; i < sc.length/2; i++) {
+                char firstChar = sc[i];
+                char secondChar = sc[sc.length - 1 - i];
+
+                sc[i] = secondChar;
+                sc[sc.length - 1 - i] = firstChar;
             }
-            // for (int i = 0; i < ca.length; i++) {
-            //     System.out.println(ca[i]);
-            // }
-            res.append(String.valueOf(ca));
-            
-            if (j != sl.length - 1) {
-                res.append(" ");
+
+            for (int i = 0; i < sc.length; i++) {
+                sb.append(sc[i]);
+            }
+            count++;
+            if (count < strs.length) {
+                sb.append(" ");
             }
         }
-        return res.toString();
+        // System.out.println(sb.toString());
+        return sb.toString();
+
     }
 }
