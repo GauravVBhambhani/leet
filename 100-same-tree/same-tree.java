859d3600bf62c35d6f1c968d74ec04df24d828d3
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2024-11-13 18:11:32 -0500

    Time: 0 ms (100.00%) | Memory: 41 MB (56.76%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-
+/*
     private void preorder(TreeNode root, ArrayList<String> arr) {
         if (root == null) {
             arr.add("null");
@@ -38,14 +38,16 @@ class Solution {
         preorder(q, q_list);
 
         return p_list.equals(q_list);
-        // int count = 0;
-        // if (p_list.size() == q_list.size()) {
-        //     for(int i = 0; i < p_list.size(); i++) {
-        //         if (p_list.get(i) == q_list.get(i)){
-        //             count++;
-        //         }
-        //     }
-        // }
-        // return (count == p_list.size());
+
+    }
+    */
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
     }
 }
