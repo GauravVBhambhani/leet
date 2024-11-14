d888d6a9f29a240cceca345d9480874e41702e10
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2024-11-14 07:59:35 -0500

    Time: 20 ms (12.93%) | Memory: 44.8 MB (20.62%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -8,7 +8,6 @@ class Solution {
             hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0)+1);
         }
 
-
         for (int i = 0; i < t.length(); i++) {
             if (!hm.containsKey(t.charAt(i)) || hm.get(t.charAt(i)) == 0) {
                 return false;
