dabda908628f39d7dbca333ab5998b155b5df48b
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-02 20:20:14 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (71.30%) - LeetSync

---
+++ 66-plus-one/plus-one.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func plusOne(_ digits: [Int]) -> [Int] {
+        // no need to convert to Int. Large Int can cause issues. Treating this as a array manipulation problem.
+
+        var digits = digits
+        // for digit in digits.indices.reversed() {}
+        for digit in (0..<digits.count).reversed() {
+            guard digits[digit] == 9 else {
+                digits[digit] += 1
+                return digits
+            }
+
+            digits[digit] = 0
+        }
+        digits.insert(1, at: 0)
+        return digits
+    }
+}
