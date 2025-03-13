74f980d1358379775b7ea1d26a5fe4816301562e
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-13 09:18:00 -0400

    Add root-equals-sum-of-children.cpp

---
+++ root-equals-sum-of-children.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func checkTree(_ root: TreeNode?) -> Bool {
+
+        guard let left = root?.left?.val else { return false }
+        guard let right = root?.right?.val else { return false }
+        
+        let sum =  left + right
+        return sum == root?.val ? true : false
+        
+    }
+}
