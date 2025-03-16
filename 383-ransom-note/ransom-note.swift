0733b5ce2bd898620695ac7f77fcf026ab08df6c
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-16 16:48:39 -0400

    Time: 7 ms (95.89%) | Memory: 20.1 MB (43.02%) - LeetSync

--- 383-ransom-note/ransom-note.swift
+++ 383-ransom-note/ransom-note.swift
@@ -1,18 +1,42 @@
 class Solution {
     func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+        // var dict = [Character: Int]()
+        // for character in ransomNote {
+        //     dict[character, default: 0] += 1
+        // }
+
+        // for character in magazine {
+        //     dict[character, default: 0] += 1
+        // }
+
+        // for (_, count) in dict {
+        //     // print("\(character) : \(count)")
+        //     if count > 0 {
+        //         return false
+        //     }
+        // }
+
+        // return true
+
         var dict = [Character: Int]()
+
         for character in ransomNote {
             dict[character, default: 0] += 1
         }
+
         for character in magazine {
-            dict[character, default: 0] -= 1
-        }
-        for (character, count) in dict {
-            print("\(character) : \(count)")
-            if count > 0 {
-                return false
+            if let count = dict[character] {
+                dict[character] = count - 1
+                if dict[character] == 0 {
+                    dict.removeValue(forKey: character)
+                }
+            }
+            if dict.isEmpty {
+                return true
             }
         }
-        return true
+
+        return dict.isEmpty
+
     }
 }
