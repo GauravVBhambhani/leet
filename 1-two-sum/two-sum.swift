c912f66c2a2d84894cfd3761bd2ede733b840d0c
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-17 17:23:14 -0400

    Time: 25 ms (27.42%) | Memory: 19.6 MB (89.31%) - LeetSync

--- 1-two-sum/two-sum.swift
+++ 1-two-sum/two-sum.swift
@@ -1,32 +1,15 @@
 class Solution {
-    // func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        
-    //     for (index, num) in nums.enumerated() {
-    //         let diff = target - num
-
-    //         for (i, value) in nums.enumerated() {
-    //             if diff == value, i != index {
-    //                 return [index, i]
-    //             }
-    //         }
-    //     }
-    //     return []
-    // }
-
-
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var hashMap = [Int : Int]()
 
-        for (index, num) in nums.enumerated() {
-            let diff = target - num
-
-            if let i = hashMap[diff] {
-                return [i, index]
+        var result: [Int] = []
+        
+        for i in 0..<nums.count {
+            for j in i+1..<nums.count {
+                if (target - nums[i] == nums[j]) {
+                    return [i, j]
+                }
             }
-
-            hashMap[num] = index
         }
         return []
     }
-
 }
