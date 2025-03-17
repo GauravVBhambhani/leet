b4de8053a3bc4b2baedac7babec99792f32cd31d
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-17 18:21:07 -0400

    Time: 2 ms (47.49%) | Memory: 19.9 MB (59.01%) - LeetSync

--- 1-two-sum/two-sum.swift
+++ 1-two-sum/two-sum.swift
@@ -1,15 +1,24 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        // for i in 0..<nums.count {
+        //     for j in i+1..<nums.count {
+        //         if (target - nums[i] == nums[j]) {
+        //             return [i, j]
+        //         }
+        //     }
+        // }
+        // return []
 
-        var result: [Int] = []
-        
+        var hm: [Int: Int] = [:]
+
         for i in 0..<nums.count {
-            for j in i+1..<nums.count {
-                if (target - nums[i] == nums[j]) {
-                    return [i, j]
-                }
+            let complement = target - nums[i]
+            if let exist = hm[complement], exist != i {
+                return [i, exist]
             }
+            hm[nums[i]] = i
         }
         return []
+
     }
 }
