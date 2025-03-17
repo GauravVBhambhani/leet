09bf5393ff58dd65660a2940b1bcef533971a97f
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-17 18:44:45 -0400

    Time: 17 ms (10.34%) | Memory: 20.3 MB (6.90%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func divideArray(_ nums: [Int]) -> Bool {
+        
+        var hm = [Int: Int]()
+
+        for num in nums {
+            hm[num, default: 0] += 1 
+        }
+
+        print(hm)
+
+        for (num, count) in hm {
+            if count % 2 != 0 {
+                return false
+            }
+        }
+
+        return true
+
+    }
+}
