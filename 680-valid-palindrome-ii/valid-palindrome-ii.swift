63c6d4c03fd538802c739466745baa32064e0500
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-19 17:11:48 -0400

    Time: 41 ms (83.92%) | Memory: 21.2 MB (19.61%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.swift
+++ 680-valid-palindrome-ii/valid-palindrome-ii.swift
@@ -4,7 +4,6 @@ class Solution {
         var j = s.count - 1
         let s = Array(s)
         while (i < j) {
-            
             if s[i] != s[j] {
                 return (checkPalindrome(s, i, j-1) || checkPalindrome(s, i+1, j))
             }
@@ -15,13 +14,12 @@ class Solution {
     }
 
     func checkPalindrome(_ s: [Character], _ i: Int, _ j: Int) -> Bool {
-        var l = s.index(s.startIndex, offsetBy: i)
-        var r = s.index(s.startIndex, offsetBy: j)
-
-        while (l < r) {
-            if s[l] != s[r] { return false }
-            l = s.index(after: l)
-            r = s.index(before: r)
+        var i = i
+        var j = j
+        while (i < j) {
+            if s[i] != s[j] { return false }
+            i += 1
+            j -= 1
         }
         return true
     }
