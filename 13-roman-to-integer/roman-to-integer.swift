b6142fadef1c86f4316ab9ad1b928528ba2baf93
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-19 15:02:55 -0400

    Time: 1 ms (79.84%) | Memory: 19.4 MB (95.76%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.swift
+++ 13-roman-to-integer/roman-to-integer.swift
@@ -1,35 +1,21 @@
 class Solution {
     func romanToInt(_ s: String) -> Int {
-        
-        var str = Array(s.reversed())
-        var count = getIntOfRoman(str[0]) 
-        for i in 1..<str.count {
-            print("Count: \(count)")
-            if getIntOfRoman(str[i]) >= getIntOfRoman(str[i-1]) {
-                count += getIntOfRoman(str[i])
-            } else {
-                count -= getIntOfRoman(str[i])
-            }
-        }
-        return count
-    }
+        let romanMap: [Character: Int] = [
+            "I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000
+        ]
 
-    func getIntOfRoman(_ c: Character) -> Int {
-        switch c {
-            case "I":
-            return 1
-            case "V":
-            return 5
-            case "X":
-            return 10
-            case "L":
-            return 50
-            case "C":
-            return 100
-            case "D":
-            return 500
-            default:
-            return 1000
+        var total = 0
+        var preValue = 0
+
+        for char in s.reversed() {
+            let value = romanMap[char] ?? 0
+            if value >= preValue {
+                total += value
+            } else {
+                total -= value
+            }
+            preValue = value
         }
+        return total
     }
 }
