e5154b2e91b2ed455c7303e2d925e55fbc9fcfc7
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-20 19:06:56 -0400

    Time: 0 ms (100.00%) | Memory: 20.7 MB (34.41%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.swift
+++ 485-max-consecutive-ones/max-consecutive-ones.swift
@@ -3,7 +3,6 @@ class Solution {
         var count = 0
         var maxCount = 0
         for num in nums {
-            print("num = \(num)")
             if num == 1 {
                 count += 1
             } else {
