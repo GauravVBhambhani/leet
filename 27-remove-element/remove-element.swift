f4132fa50ed627a4fb4c1ad84c1a58c1d79c6a36
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-25 18:32:09 -0400

    Time: 0 ms (100.00%) | Memory: 20 MB (17.32%) - LeetSync

--- 27-remove-element/remove-element.swift
+++ 27-remove-element/remove-element.swift
@@ -1,13 +1,16 @@
 class Solution {
     func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
         
-        var i = 0
-        for num in nums {
-            if num != val {
-                nums[i] = num
-                i += 1
-            }
-        }
-        return i
+        // var i = 0
+        // for num in nums {
+        //     if num != val {
+        //         nums[i] = num
+        //         i += 1
+        //     }
+        // }
+        // return i
+
+        nums = nums.filter { $0 != val }
+        return nums.count
     }
 }
