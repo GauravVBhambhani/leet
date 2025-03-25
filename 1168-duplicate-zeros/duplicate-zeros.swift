1d385f2ecb216030eee546e5ea2585409c0fd851
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-25 16:47:27 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (90.76%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.swift
+++ 1168-duplicate-zeros/duplicate-zeros.swift
@@ -1,21 +1,42 @@
 class Solution {
     func duplicateZeros(_ arr: inout [Int]) {
+        
+        // 1: I need to find the number of zeroes we will be duplicating (without exceeding boundary)
+        var zerosToDuplicate = 0
+        var n = arr.count
 
-        let n = arr.count
-        var results = [Int](repeating: 0, count: n)
-        var j = 0
-
-        for i in (0 ..< n) {
+        for i in 0 ..< n {
             if arr[i] == 0 {
-                j += 1
-            } else {
-                results[j] = arr[i]
-            }
-            j += 1
+                if i + zerosToDuplicate >= n-1 {
 
-            if (j >= n) { break }
+                    // handles if last element is 0 => not to be duplicated
+                    if i + zerosToDuplicate == n-1 {
+                        arr[n-1] = 0
+                        n -= 1
+                    }
+
+                    break
+                }
+
+                zerosToDuplicate += 1
+            }
         }
-        print(results)
-        arr = results
+
+        // 2: make inline changes by 2 pointer from left to right
+
+        var i = n - 1 - zerosToDuplicate
+        var j = n - 1
+
+        while i >= 0 {
+            if arr[i] == 0 {
+                arr[j] = 0
+                j -= 1
+                arr[j] = 0
+            } else {
+                arr[j] = arr[i]
+            }
+            i -= 1
+            j -= 1
+        }
     }
 }
