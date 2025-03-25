542e292ed46944151c7601816993404ebf90a2d8
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-03-24 20:56:31 -0400

    Time: 0 ms (100.00%) | Memory: 21.1 MB (44.55%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.swift
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.swift
@@ -1,12 +1,28 @@
 class Solution {
     func sortedSquares(_ nums: [Int]) -> [Int] {
-        
-        var nums = nums
-        for i in 0..<nums.count {
-            nums[i] *= nums[i]
+        // var nums = nums
+        // for i in 0..<nums.count {
+        //     nums[i] *= nums[i]
+        // }
+        // return nums.sorted()
+
+        var n = nums.count
+        var result = [Int](repeating: 0, count: n)
+
+        var l = 0
+        var r = nums.count - 1
+
+        for i in (0 ..< n).reversed() {
+            if ( abs(nums[l]) > abs(nums[r]) ) {
+                result[i] = nums[l] * nums[l]
+                l += 1
+                
+            } else {
+                result[i] = nums[r] * nums[r]
+                r -= 1
+            }
         }
 
-        return nums.sorted()
-
+        return result
     }
 }
