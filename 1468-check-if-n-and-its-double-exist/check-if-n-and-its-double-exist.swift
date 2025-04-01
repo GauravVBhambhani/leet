3cc06eeccd646d3e4e8e582ed4c5b6f113235efd
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-04-01 17:18:58 -0400

    Time: 1 ms (22.43%) | Memory: 19.4 MB (82.24%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.swift
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.swift
@@ -7,17 +7,17 @@ class Solution {
 
             while (l <= r) {
                 var mid = l + (r - l) / 2
-                
-                if mid != i && arr[mid] == 2 * arr[i] { return true }
+                if arr[mid] == 2 * arr[i] && mid != i {
+                    return true
+                }
 
-                if arr[mid] < 2 * arr[i] {
-                    l = mid + 1
-                } else {
+                if arr[mid] > 2 * arr[i] {
                     r = mid - 1
+                } else {
+                    l = mid + 1
                 }
             }
         }
-
         return false
     }
 }
