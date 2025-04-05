109259fa7ee0a885b1b4bb7988836999d9f97934
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-04-05 19:49:30 -0400

    Time: 0 ms (100.00%) | Memory: 19.9 MB (69.44%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.swift
+++ 941-sort-array-by-parity/sort-array-by-parity.swift
@@ -1,22 +1,34 @@
 class Solution {
     func sortArrayByParity(_ nums: [Int]) -> [Int] {
-        var res: [Int] = Array(repeating: 0, count: nums.count)
+        // var res: [Int] = Array(repeating: 0, count: nums.count)
+        // var i = 0
+        // for even in 0..<nums.count {
+        //     if nums[even] % 2 == 0 {
+        //         res[i] = nums[even]
+        //         i += 1
+        //     }
+        // }
+        // for odd in 0..<nums.count {
+        //     if nums[odd] % 2 != 0 {
+        //         res[i] = nums[odd]
+        //         i += 1
+        //     }
+        // }
+        // return res
+
+        var nums = nums
         var i = 0
-
-        for even in 0..<nums.count {
-            if nums[even] % 2 == 0 {
-                res[i] = nums[even]
-                i += 1
+        var j = nums.count - 1
+        while (i < j) {
+            if (nums[i]%2 > nums[j]%2) {
+                var temp = nums[j]
+                nums[j] = nums[i]
+                nums[i] = temp
             }
-        }
 
-        for odd in 0..<nums.count {
-            if nums[odd] % 2 != 0 {
-                res[i] = nums[odd]
-                i += 1
-            }
+            if nums[i] % 2 == 0 { i += 1}
+            if nums[j] % 2 != 0 { j -= 1}
         }
-        
-        return res
+        return nums
     }
 }
